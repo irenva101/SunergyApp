daf9404a88d657a0a30e854eb7cc53849938e266
Irena Varga <irenva101@gmail.com>
2024-12-28 22:00:10 +0100

    client for FE

--- Sunergy.Ng/src/app/app.config.ts
+++ Sunergy.Ng/src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { environment } from './environments/environment';
+import { API_BASE_URL } from './api/api-reference';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    { provide: API_BASE_URL, useValue: environment.pathApi}
+  ],
 };
---
+++ Sunergy.Ng/src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment={
+    production: false,
+    pathApi: 'https://localhost:44366/',
+    environmentName: 'local',
+};
